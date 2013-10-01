65ecc50d50f2e405c41e5bf5de581782cd1bde61
Dmitry <kangreon@gmail.com>
2013-10-01 22:48:52 +0300

    Fixed cleaning in folders with spaces

--- clean.bat
+++ clean.bat
@@ -2,12 +2,12 @@
 
 set REMOVE_EXTENTIONS=*.db *.o *.ppu *.a *.res~ *.dcu *.ddp *.local *.identcache *.exe *.or *.compiled *.cfg *.dbg *.dof *.lps *.res
 
-for /F %%A in ('dir %REMOVE_EXTENTIONS% /b /s /a:-d') do (
-	del /Q %%A
+for /F "delims=" %%A in ('dir %REMOVE_EXTENTIONS% /b /s /a:-d') do (
+	del /Q "%%A"
 )
 
-for /F %%A in ('dir *__history *backup /b /s /a:D') do (
-	rd %%A /S /Q
+for /F "delims=" %%A in ('dir *__history *backup /b /s /a:D') do (
+	rd "%%A" /S /Q
 )
 
 pause
